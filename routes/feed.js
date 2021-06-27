bdab524e316280ca49381d5479cd246e3cd91f54
Sara Anwar <saraanwar97@gmail.com>
2021-06-27 14:54:23 +0200

    added server-side validation

--- routes/feed.js
+++ routes/feed.js
@@ -1,7 +1,13 @@
 const express = require('express');
+const {body} = require('express-validator/check');
 const feedController = require('../controllers/feed');
 const router = express.Router();
 //   /feed/posts
 router.get('/posts', feedController.getPosts);
-router.post('/post',feedController.postPosts);
+router.post('/post',
+[
+    body('title').trim().isLength({ min:5 }),
+    body('content').trim().isLength({ min:5 })
+],
+feedController.postPosts);
 module.exports = router;
