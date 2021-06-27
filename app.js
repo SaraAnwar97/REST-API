b1dddcfff314cd935991cb8c2b40802c25a23816
Sara Anwar <saraanwar97@gmail.com>
2021-06-27 21:21:06 +0200

    upload images

--- app.js
+++ app.js
@@ -5,8 +5,29 @@ const mongoose = require('mongoose');
 const MONGODB_URI = 'mongodb+srv://Sara:Ss4923@@@cluster0.ldpfv.mongodb.net/Feed';
 const app = express();
 const feedRoutes = require('./routes/feed');
+const multer = require('multer');
+
+const fileStorage = multer.diskStorage({
+  destination: (req,file,cb)  =>{
+    cb(null, 'images'); // null: no error
+  },
+  filename: (req,file,cb) => {
+    cb(null , new Date().toISOString() + '-' + file.originalname);
+  }
+});
+
+const fileFilter = (req,file,cb) => {
+  if(file.mimetype === 'image/png' || 
+  file.mimetype === 'image/jpg' ||
+   file.mimetype === 'image/jpeg' ) {
+    cb(null, true);
+  } else {
+    cb(null, false);
+  }
+};
 
 app.use(bodyParser.json()); // application/json in header
+app.use(multer({ storage: fileStorage , fileFilter: fileFilter }).single('image'));
 app.use('/images',express.static(path.join(__dirname,'images')));
 app.use((req,res,next)=>{
 res.setHeader('Access-Control-Allow-Origin','*');
@@ -17,7 +38,7 @@ next();
 app.use('/feed',feedRoutes);
 app.use((error,req,res,next)=>{
   console.log(error);
-  const status = error.statsuCode || 500 ;
+  const status = error.statusCode || 500 ;
   const message = error.message; //by default
   res.status(status).json({message:message});
 })
