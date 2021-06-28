03b63cdf1cabf6d9e338cca1643d4f2d5c090d76
Sara Anwar <saraanwar97@gmail.com>
2021-06-28 15:16:02 +0200

    updating posts

--- controllers/feed.js
+++ controllers/feed.js
@@ -1,5 +1,8 @@
 const {validationResult} = require('express-validator/check');
+const { ContextHandlerImpl } = require('express-validator/src/chain');
 const Post = require('../models/post');
+const path = require('path');
+const fs = require('fs');
 exports.getPosts = (req,res,next)=>{
     Post.find()
     .then(posts =>{
@@ -69,4 +72,54 @@ exports.getPost = (req,res,next) =>{
         }
         next (err);
     });
+ };
+
+ exports.updatePost = (req,res,next) =>{
+     const postId = req.params.postId;
+     const errors = validationResult(req);
+    if(!errors.isEmpty()){
+        const error = new Error('Validation failed,entered data is incorrect');
+        error.statusCode = 422;
+        throw error;
+    }
+     const title = req.body.title;
+     const content = req.body.content;
+     let imageUrl = req.body.image;
+     if(req.file){
+         imageUrl = req.file.path;
+     }
+     if(!imageUrl){
+         const error = new Error('No file picked');
+         error.statusCode = 422;
+         throw error;
+     }
+     Post.findById(postId)
+     .then(post =>{
+         if(!post){
+             const error = new Error('Could not find post');
+             error.statusCode = 404;
+             throw error;
+         }
+         //if new extracted path is not the old path , so it's a new path(img)
+         if(imageUrl !== post.imageUrl){
+             clearImage(post.imageUrl); // pass old path as an argument
+         }
+         post.title = title;
+         post.content = content;
+         post.imageUrl = imageUrl;
+         return post.save();
+     }).then(result =>{
+        return res.status(200).json({message: 'Post updated', post:result});
+     })
+     .catch(err => {
+        if(!err.statusCode){
+            err.statusCode = 500;
+        }
+        next (err);
+    });
+ }
+
+ const clearImage = filePath =>{
+     filePath = path.join(__dirname, '..', filePath);
+     fs.unlink(filePath , err => console.log(err));
  };
