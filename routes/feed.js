a4924ec96472864a9987bd8096c872fb45b543b7
Sara Anwar <saraanwar97@gmail.com>
2021-06-28 20:37:37 +0200

    Added Auth middleware to all routes

--- routes/feed.js
+++ routes/feed.js
@@ -5,17 +5,17 @@ const isAuth = require('../middleware/is-auth');
 const router = express.Router();
 //   /feed/posts
 router.get('/posts', isAuth, feedController.getPosts);
-router.post('/post',
+router.post('/post',isAuth,
 [
     body('title').trim().isLength({ min:5 }),
     body('content').trim().isLength({ min:5 })
 ],
 feedController.postPosts);
-router.get('/post/:postId', feedController.getPost);
-router.put('/post/:postId',[
+router.get('/post/:postId',isAuth, feedController.getPost);
+router.put('/post/:postId',isAuth,[
     body('title').trim().isLength({ min:5 }),
     body('content').trim().isLength({ min:5 })
 ] ,
 feedController.updatePost);
-router.delete('/post/:postId',feedController.deletePost);
+router.delete('/post/:postId',isAuth,feedController.deletePost);
 module.exports = router;
