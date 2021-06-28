02571dba653f4c92b7bc89c41f82d48ba32e6f63
Sara Anwar <saraanwar97@gmail.com>
2021-06-28 20:32:28 +0200

    using & validating jwt

--- routes/feed.js
+++ routes/feed.js
@@ -1,9 +1,10 @@
 const express = require('express');
 const {body} = require('express-validator/check');
 const feedController = require('../controllers/feed');
+const isAuth = require('../middleware/is-auth');
 const router = express.Router();
 //   /feed/posts
-router.get('/posts', feedController.getPosts);
+router.get('/posts', isAuth, feedController.getPosts);
 router.post('/post',
 [
     body('title').trim().isLength({ min:5 }),
