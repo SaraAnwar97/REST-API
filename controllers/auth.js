90b8437dc87462ad5f0a3933c03e7d54211340f6
Sara Anwar <saraanwar97@gmail.com>
2021-06-28 18:29:43 +0200

    signing users up

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,4 +1,5 @@
 const User = require('../models/user');
+const bcrypt = require('bcryptjs');
 const {validationResult} = require('express-validator/check');
 
 exports.signup = (req,res,next) =>{
@@ -12,4 +13,22 @@ if(!errors.isEmpty()){
 const email = req.body.email;
 const name = req.body.name;
 const password = req.body.password;
+bcrypt.hash(password,12)
+.then(hashedPw =>{
+    const user = new User({
+        email: email,
+        password: hashedPw,
+        name:name
+    });
+    return user.save();
+}).then(result =>{
+    res.status(201).json({ message: 'User created',
+     userId : result._id}); //result: User mongoose object
+})
+.catch(err => {
+    if(!err.statusCode){
+        err.statusCode = 500;
+    }
+    next (err);
+});
 };
